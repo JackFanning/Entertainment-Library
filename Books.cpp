9e8d58714ec66f9bb554a7a5b2ee9479f709f0e7
JackFanning <c23722731@mytudublin.ie>
2025-02-20 11:53:33 +0000

    Update Books.cpp

--- Books.cpp
+++ Books.cpp
@@ -6,12 +6,25 @@ Books::Books() : current_page(0)
     //ctor
 }
 
-Books::Books(string nam, string gen, string pub)
+Books::Books(string nam, string gen, string pub, int curPage)
 {
     name = nam;
     genre = gen;
     publisher = pub;
+    current_page = curPage;
 }
+
+void Books::setBookDetails(int cp) {
+
+    if (cp < 0){
+        cout << "Current page should be more than 0! Setting default to Page 1.. \n" << endl;
+        current_page = 1;
+    }
+    else {
+        current_page = cp;
+    }
+}
+
 
 void Books::display()
 {
