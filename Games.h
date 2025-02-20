909944d44f999b16de379e8fee3d3dbe8d99c21d
JackFanning <c23722731@mytudublin.ie>
2025-02-20 11:54:30 +0000

    Update Games.h

--- Games.h
+++ Games.h
@@ -1,4 +1,3 @@
-
 #ifndef GAMES_H
 #define GAMES_H
 #include "ParentClass.h"
@@ -9,18 +8,13 @@ public:
     Games();
     void setHoursPlayed(float hp);
     void setstorageSize(float s);
-    void setGameDetails(string n, string g, float hp, float s);
+    void setGameDetails(float hp, float s);
     void displayDetails();
 
     ~Games();
 
-    
     void display();
-
 private:
-
-    string name;
-    string genre;
     float hoursPlayed;
     float storageSize;
 
