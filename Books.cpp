a48bb413ac25aca2ab688243dabd8daf74085c6f
BilalS31 <bilal.salah316@gmail.com>
2025-02-26 20:46:05 +0000

    Update Books.cpp

--- Books.cpp
+++ Books.cpp
@@ -16,7 +16,7 @@ Books::Books(string nam, string gen, int curPage)        //Overloaded constructo
 void Books::setBookDetails(int cp) {
 
     if (cp < 0){
-        cout << "Current page should be more than 0! Setting default to Page 1.. \n" << endl;
+        cout << "Current page should be more than 0! Setting default to Page 1.. \n" << endl;       //Error checking so the current page value isn't negative
         current_page = 1;
     }
     else {
