ac2ff9a78ebad964485a44c3440fd30ee4c90733
BilalS31 <bilal.salah316@gmail.com>
2025-02-26 12:53:53 +0000

    Update ParentClass.h

--- ParentClass.h
+++ ParentClass.h
@@ -3,21 +3,21 @@
 #include "Publisher.h"
 
 
-class ParentClass: public Publisher
+class ParentClass: public Publisher                 //Implementation of inheritance relationship: ParentClass is inhereting from the Publisher Class
 {
     public:
         ParentClass();
         virtual ~ParentClass();
 
         void display();
-        void setDetails(string n, string g);
+        void setDetails(string n, string g);            //Data integrity: mutator methods
 
         string name;
         string genre;
 
         Publisher publisher;
         Publisher getPublisher();
-        void setPublisher(Publisher p);
+        void setPublisher(Publisher p);                //Data integrity: mutator methods
     protected:
 
     private:
