98071720036acce31939ec7fbb8995ccbe84ec60
JackFanning <c23722731@mytudublin.ie>
2025-02-27 11:49:52 +0000

    Update ParentClass.h

--- ParentClass.h
+++ ParentClass.h
@@ -2,6 +2,7 @@
 #define PARENTCLASS_H
 #include "Publisher.h"
 
+const int MAX_P = 2;
 
 class ParentClass: public Publisher                 //Implementation of inheritance relationship: ParentClass is inhereting from the Publisher Class
 {
@@ -15,12 +16,13 @@ class ParentClass: public Publisher                 //Implementation of inherita
         string name;
         string genre;
 
-        Publisher publisher;
         Publisher getPublisher();
-        void setPublisher(Publisher p);                //Data integrity: mutator methods
+        bool setPublisher(Publisher* p);                //Data integrity: mutator methods
     protected:
 
     private:
+        int p_cnt=0;
+        Publisher* publisher[MAX_P];
 };
 
 #endif // PARENTCLASS_H
