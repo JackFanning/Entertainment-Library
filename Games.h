0a1d18f574d0428e12d8767d1b2f8e420824d725
BilalS31 <bilal.salah316@gmail.com>
2025-02-28 23:16:09 +0000

    Update Games.h

--- Games.h
+++ Games.h
@@ -6,15 +6,17 @@
 class Games: public MediaItem          //Implemenation of inheritence relationship: games class is inhereting from MediaItem
 {
 public:
-    Games();
+    Games();                            // default parameterless constructor
+
+    void display();
+
     void setHoursPlayed(float hp);                       //data integrity checks (mutators)
     void setstorageSize(float s);                        //data integrity checks (mutators)
-    void setGameDetails(float hp, float s);              //data integrity checks (mutators)
-    void displayDetails();
+    void setGameDetails(string n, string g, float hp, float s);
+    void setGameDetails(float hp, float s); // data integrity checks (mutators)
 
     ~Games();
 
-    void display();
 private:   //data hiding
     float hoursPlayed;
     float storageSize;
