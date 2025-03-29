47fdb171451c8d5b045817eb6b30f6b2f3f5d683
BilalS31 <bilal.salah316@gmail.com>
2025-03-29 19:17:11 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,5 +30,48 @@ int main()
         cout << "b2 is greater than b3";
     }
 
+
+//Deep copying
+    Publisher* p1 = new Publisher(10,"Aaa","2003","Up","Rel");
+
+
+    Books b1("Harry Potter","Action",23,100,p1);
+
+    b1.display();
+
+    Books b2 = b1;  //Deep copy constructor
+    
+    b2.setName("Top");
+    b2.publisher->SetnamePublisher("BBB");
+    b2.display();
+
+    cout << "Original b1: " << endl;
+    b1.display();
+
+
+    Books b3;
+    b3 = b1;
+    b3.setName("Differenet");
+    b3.display();
+
+
+
+
+    Publisher* p2 = new Publisher(3,"Mojang","2009","Coming","Out");
+    Games g1("Minecraft","Adventure",13,20,p2);
+
+    g1.display();
+
+
+    Games g2 = g1;  //Deep copy constructor
+    g2.setName("X");
+    g2.setstorageSize(6);
+    g2.display();
+
+    g1.display();
+
+
+
+    
     return 0;
 }
